dcf25018ef4d22ee70eb917f5b08b0294dadaa44
ecneho <ecneho@protonmail.com>
2026-06-11 03:25:23 +0300

    feat: networking wip

--- common/media/lua/client/TimedActions/ISEatFoodOverride.lua
+++ common/media/lua/client/TimedActions/ISEatFoodOverride.lua
@@ -3,7 +3,7 @@ local original_ISEatFoodAction_new = ISEatFoodAction.new
 function ISEatFoodAction:new(character, item, percentage)
     local o = original_ISEatFoodAction_new(self, character, item, percentage)
     if item:getEatType() == "Injector" then
-        o.maxTime = 10
+        o.maxTime = 10 -- TODO: add a variable
     end
     return o
 end
